adddd2751f2256ad90ebf275a93dc8eff37ca177
frvnzz <142498031+frvnzz@users.noreply.github.com>
2026-04-22 18:11:32 +0200

    Fix: update site name and URL in configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://frvnzz.github.io/project-venus/",
-  base: "/project-venus/",
+  site: "https://frvnzz.github.io/i-miss-you-so-much/",
+  base: "/i-miss-you-so-much/",
   devToolbar: {
     enabled: false,
   },
